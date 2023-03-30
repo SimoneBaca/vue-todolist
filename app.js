6044a69d0722bc2575883cee536525274aeccbf9
Simonebaca <simobaco99@gmail.com>
2023-03-30 19:31:40 +0200

    Milestone 3

--- app.js
+++ app.js
@@ -18,43 +18,35 @@ createApp({
         done: false,
         text: "",
       },
-      todoList: [
-        {
-          done: false,
-          text: "Flour",
-        },
-        {
-          done: false,
-          text: "Bread",
-        },
-        {
-          done: false,
-          text: "Eggs",
-        },
-        {
-          done: false,
-          text: "Milk",
-        },
-        {
-          done: false,
-          text: "Sugar",
-        },
-      ],
+      todoList: [],
     };
   },
   methods: {
     done(index) {
       if (this.todoList[index].done === false) {
         this.todoList[index].done = true;
-        console.log('fatto');
+        console.log("fatto");
       } else if (this.todoList[index].done === true) {
-        this.todoList[index].done === false; 
-        console.log('da fare');
+        this.todoList[index].done === false;
+        console.log("da fare");
       }
+    },
+    removeTodo(index) {
+      this.todoList.splice(index, 1);
+    },
+    addTodo() {
+      if (this.newText.trim() !== "") {
+        let { text, done } = this.todoInfo;
+        text = this.newText;
+        this.todoList.push({
+          text,
+          done,
+        });
+        this.newText = "";
+      }
+      
 
     },
-    removeTodo(index){
-        this.todoList.splice(index,1);
-    }
+   
   },
 }).mount("#app");
